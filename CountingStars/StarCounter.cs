1eac86495e642689adad85d6fa2e17c38bd5e558
Pontus Liljeblad <liljebladpontus@gmail.com>
2023-04-20 16:53:20 +0200

    Make code run and count number of stars in each map

--- CountingStars/StarCounter.cs
+++ CountingStars/StarCounter.cs
@@ -5,9 +5,53 @@ public class StarCounter
     public int Count(string[] map)
     {
         var counter = 0;
+        var visited = new bool[map.Length, GetMaxRowLength(map)];
 
-        //Please insert code
+        for (int i = 0; i < map.Length; i++) 
+        {
+            for(int j = 0; j < map[i].Length; j++)
+            {
+                if (visited[i, j])
+                {
+                    continue; 
+                }
+
+                if (map[i][j] == '*')
+                {
+                    counter++; 
+                    TraverseStar(map, visited, i, j);
+                }
+            }
+        }
 
         return counter;
+    }
+
+    private static int GetMaxRowLength(string[] map) 
+    {
+        var longest = map.Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur); 
+
+        return longest.Length;
+    }
+
+    private static void TraverseStar(string[] map, bool[,] visited, int i, int j)
+    {
+        if(i < 0 || i >= map.Length || j < 0 || j >= map[i].Length)
+        {
+            return; 
+        }
+
+        if (visited[i, j] || map[i][j] == '.') 
+        {
+            return; 
+        }
+
+        visited[i, j] = true;
+
+        TraverseStar(map, visited, i - 1, j);
+        TraverseStar(map, visited, i + 1, j);
+        TraverseStar(map, visited, i, j - 1);
+        TraverseStar(map, visited, i, j + 1);
+
     }
 }
